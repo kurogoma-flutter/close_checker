464e66d99242ecb381e06b069f7930ab37f72621
Kurogoma939 <kan.sss.939@gmail.com>
2022-08-28 18:19:04 +0900

    設定画面にアイコン追加

--- lib/presentation/pages/setting/setting_page.dart
+++ lib/presentation/pages/setting/setting_page.dart
@@ -16,27 +16,87 @@ class SettingPage extends HookConsumerWidget {
             },
             child: const ListTile(
               title: Text('このアプリについて'),
-              trailing: Icon(Icons.arrow_forward),
+              trailing: _RightArrowIcon(),
             ),
           ),
           const Divider(),
-          const ListTile(
-            title: Text('フォントサイズ'),
-            trailing: Icon(Icons.arrow_forward),
+          ListTile(
+            title: const Text('フォントサイズ'),
+            trailing: SizedBox(
+              height: 48,
+              child: GestureDetector(
+                onTap: () {
+                  // TODO: フォントサイズ変更
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(6),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade100,
+                    border: Border.all(
+                      color: Colors.blueAccent,
+                    ),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Image.asset(
+                    'assets/font-style-icon.png',
+                  ),
+                ),
+              ),
+            ),
           ),
           const Divider(),
-          const ListTile(
-            title: Text('アプリテーマ'),
-            trailing: Icon(Icons.arrow_forward),
+          ListTile(
+            title: const Text('アプリテーマ'),
+            trailing: SizedBox(
+              height: 48,
+              child: GestureDetector(
+                onTap: () {
+                  // TODO: フォントサイズ変更
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(6),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade100,
+                    border: Border.all(
+                      color: Colors.blueAccent,
+                    ),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Image.asset(
+                    'assets/app-theme.png',
+                  ),
+                ),
+              ),
+            ),
           ),
           const Divider(),
           GestureDetector(
             onTap: () {
               context.go('/setting/pin');
             },
-            child: const ListTile(
-              title: Text('PIN設定'),
-              trailing: Icon(Icons.arrow_forward),
+            child: ListTile(
+              title: const Text('PIN設定'),
+              trailing: SizedBox(
+                height: 48,
+                child: GestureDetector(
+                  onTap: () {
+                    // TODO: フォントサイズ変更
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(6),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade100,
+                      border: Border.all(
+                        color: Colors.blueAccent,
+                      ),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Image.asset(
+                      'assets/pin-code.png',
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
           const Divider(),
@@ -46,7 +106,7 @@ class SettingPage extends HookConsumerWidget {
             },
             child: const ListTile(
               title: Text('規約'),
-              trailing: Icon(Icons.arrow_forward),
+              trailing: _RightArrowIcon(),
             ),
           ),
           const Divider(),
@@ -56,11 +116,28 @@ class SettingPage extends HookConsumerWidget {
             },
             child: const ListTile(
               title: Text('問い合わせ'),
-              trailing: Icon(Icons.arrow_forward),
+              trailing: _RightArrowIcon(),
             ),
           ),
           const Divider(),
         ],
+      ),
+    );
+  }
+}
+
+class _RightArrowIcon extends StatelessWidget {
+  const _RightArrowIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 10),
+      child: SizedBox(
+        height: 28,
+        child: Image.asset(
+          'assets/right-arrow.png',
+        ),
       ),
     );
   }
