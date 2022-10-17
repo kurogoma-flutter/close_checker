029279a21cb7179bb4838594425365c10653d9fe
Kurogoma939 <kan.sss.939@gmail.com>
2022-10-18 08:32:41 +0900

    colorTheme設定のnotifier実装

---
+++ lib/domain/setting/setting_notifier.dart
@@ -0,0 +1,30 @@
+import 'package:close_checker/domain/setting/app_theme/app_theme_service.dart';
+import 'package:close_checker/domain/setting/setting_state.dart';
+import 'package:close_checker/infrastructure/repository/app_theme_repository.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+/// 設定（setting）のnotifier
+final settingNotifierProvider =
+    StateNotifierProvider.autoDispose<SettingNotifier, SettingState>(
+  (ref) => SettingNotifier(
+    appThemeService: ref.read(appThemeService),
+  ),
+);
+
+class SettingNotifier extends StateNotifier<SettingState> {
+  SettingNotifier({
+    required this.appThemeService,
+  }) : super(const SettingState());
+
+  final AppThemeService appThemeService;
+
+  /// アプリテーマをSharedPreferenceに保存する
+  Future<void> saveAppTheme(AppTheme selectedTheme) async {
+    await appThemeService.saveAppTheme(selectedTheme);
+  }
+
+  /// SharedPreferenceからアプリテーマを取得する（キャッシュに保存）
+  Future<void> getAppTheme() async {
+    await appThemeService.getAppTheme();
+  }
+}
