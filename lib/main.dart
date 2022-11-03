8174a8b4e823795479ca0587cb5a9657d511cfb5
Kurogoma939 <kan.sss.939@gmail.com>
2022-11-04 01:15:20 +0900

    ローカルDB削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:close_checker/firebase_options.dart';
-import 'package:close_checker/utility/logger.dart';
 import 'package:close_checker/presentation/router/route.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:sqflite/sqflite.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,42 +12,8 @@ Future<void> main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  // データベースを開く（なければ作成する）
-  Future<Database> getDatabase() async {
-    final path = await getDbPath();
-    final db = await openDatabase(path, version: 1,
-        onCreate: (Database db, int version) async {
-      await db.execute(
-          "CREATE TABLE major_list(listId INTEGER PRIMARY KEY, content TEXT, createdUserId TEXT, isDeleted BOOLEAN)");
-      await db.execute(
-          "CREATE TABLE minor_list(checkId INTEGER PRIMARY KEY, listId TEXT, icon TEXT, content TEXT, isDone BOOLEAN, isDeleted BOOLEAN)");
-    });
-    logger.i('データベース設定完了');
-    return db;
-  }
-
-  Future<String> getDbPath() async {
-    final dbDirectory = await getApplicationSupportDirectory();
-    final dbFilePath = dbDirectory.path;
-    final path = join(dbFilePath, 'close_checker.db');
-    return path;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    Future(() async {
-      await getDatabase();
-    });
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
